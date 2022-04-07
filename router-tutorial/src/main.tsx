10d6503513c9e3fac5ce9e4b0ae856a6fa5d7ea3
Fernando <fernandoperezdelatorremunoz@gmail.com>
2022-04-07 21:27:15 +0200

    Añadida ruta index para invoices

--- router-tutorial/src/main.tsx
+++ router-tutorial/src/main.tsx
@@ -7,23 +7,31 @@ import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />}>
-          <Route path=":invoiceId" element={<Invoice />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>¡Aqui no hay nada!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  rootElement
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="expenses" element={<Expenses />} />
+                <Route path="invoices" element={<Invoices />}>
+                    <Route
+                        index
+                        element={
+                            <main style={{ padding: "1rem" }}>
+                                <p>Aqui no hay nada</p>
+                            </main>
+                        }
+                    />
+                    <Route path=":invoiceId" element={<Invoice />} />
+                </Route>
+                <Route
+                    path="*"
+                    element={
+                        <main style={{ padding: "1rem" }}>
+                            <p>¡Aqui no hay nada!</p>
+                        </main>
+                    }
+                />
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+    rootElement
 );
